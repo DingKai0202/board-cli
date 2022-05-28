4aea00c5c2436cc1c083c7fc70866c7bc4154f15
ding kai <46352678+DingKai0202@users.noreply.github.com>
2022-05-28 14:47:36 +0800

    node最低版本检查

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -2,13 +2,31 @@
 
 module.exports = core;
 
+const semver = require('semver');
+const colors = require('colors/safe');
 const pkg = require('../package.json');
 const log = require('@board-cli/log');
+const constant = require('./const');
 
 function core() {
-  checkPkgVersion();
+  try {
+    checkPkgVersion();
+    checkNodeVersion();
+  } catch (e) {
+    log.error(e.message);  
+  }
 }
 
 function checkPkgVersion() {
   log.notice('cli', pkg.version);
+}
+
+function checkNodeVersion() {
+  // 获取当前Node版本号  
+  const currentVersion = process.version;
+  // 比对最低版本号
+  const lowestVersion = constant.LOWEST_NODE_VERSION;
+  if (!semver.gte(currentVersion, lowestVersion)) {
+    throw new Error(colors.red(`board-cli 需要安装 v${lowestVersion} 以上版本的 Node.js`));    
+  }  
 }
